b99cef83f9b5b8946966bafd8285a040fdf95a91
Sam Hellawell <sshellawell@gmail.com>
2020-06-12 21:09:26 +0100

    add initial readme and server.js file =

---
+++ README.md
@@ -0,0 +1,11 @@
+Dock VC API
+===
+TODO: update this readme
+
+Starting the server
+===
+In dev mode:
+`npm run dev`
+
+In production mode:
+`npm run start` or `forever start server.js`
---
+++ server.js
@@ -0,0 +1,19 @@
+// Require the framework
+const Fastify = require('fastify');
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+  pluginTimeout: 10000
+});
+
+// Register your application as a normal plugin.
+app.register(require('./app.js'));
+
+// Start listening.
+app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
