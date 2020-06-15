c67d5805d7f7a003c940b9be564cc225cb06ffbf
Sam Hellawell <sshellawell@gmail.com>
2020-06-15 22:49:49 +0100

    add did:dock:5ENAMn7nCVtrnXRVBSptnx6m4MrzQRKiY5AyydigCDzwPXhN unlocked DID

--- helpers/unlocked-dids/index.js
+++ helpers/unlocked-dids/index.js
@@ -2,4 +2,5 @@ module.exports = [
   require('./did_key_z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd.json'),
   require('./did_elem_ropsten_EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg.json'),
   require('./did_v1_test_nym_z6MkhdmzFu659ZJ4XKj31vtEDmjvsi5yDZG5L7Caz63oP39k.json'),
+  require('./did_dock_5ENAMn7nCVtrnXRVBSptnx6m4MrzQRKiY5AyydigCDzwPXhN.json'),
 ];
