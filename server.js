c7ef55260340689a33500b4712ec52ac14a166f3
Sam Hellawell <sshellawell@gmail.com>
2020-06-15 22:02:43 +0100

    server.js use port 80 by default

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = Fastify({
 app.register(require('./app.js'));
 
 // Start listening.
-app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
+app.listen(process.env.PORT || 80, '0.0.0.0', (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
