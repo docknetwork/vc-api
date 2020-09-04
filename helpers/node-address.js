87a5915dc6459d9ae25b8799b66b1cf974c47d03
Sam Hellawell <sshellawell@gmail.com>
2020-09-04 18:19:26 +0100

    update node address
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- helpers/node-address.js
+++ helpers/node-address.js
@@ -1,5 +1,5 @@
 // Hardcoded testnet node address for now, but provide config options in .env later
-const nodeAddress = 'wss://testnet-node.dock.io:9950';
+const nodeAddress = 'wss://danforth-1.dock.io';
 // const nodeAddress = 'ws://localhost:9944';
 
 module.exports = nodeAddress;
